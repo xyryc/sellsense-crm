ac5c6f6afbe8679769f1ed3694cb339177c7167d
hm-faisal <contact.hmfaisal@gmail.com>
2025-04-02 19:13:25 +0600

    added simple profile page

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,5 +1,6 @@
 import LogoutButton from "@/app/(auth)/_components/logout-button";
 import { auth } from "@/auth";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Profile = async () => {
@@ -8,9 +9,18 @@ const Profile = async () => {
   if (!session?.user) redirect("/login");
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <h3>Email: {session?.user?.email}</h3>
+    <div className="flex justify-center items-center flex-col gap-4 h-full">
+      <Image
+        src={
+          "https://wallpapers-clan.com/wp-content/uploads/2022/11/cute-frog-pfp-2.jpg"
+        }
+        alt="Admin Profile Picture"
+        width={100}
+        height={100}
+        className="rounded-full border-4 border-cyan-500 cursor-pointer"
+      />
+      <h1 className="text-2xl font-bold">Welcome Back!</h1>
+      <h3 className="text-gray-600">Email: {session?.user?.email}</h3>
       <LogoutButton />
     </div>
   );
