a47296432a34aff4168f7a695cf929917fa1d5fd
xyryc <amdanikzz@gmail.com>
2025-04-02 19:42:40 +0600

    dashboard layout updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,22 +3,19 @@ import ProductBarChart from "./ProductBarChart";
 import ProductLineChart from "./ProductLineChart";
 import ProductPieChart from "./ProductPieChart";
 import OverallStatsChart from "./OverallStatsChart";
-import { Calendar } from "./ui/calendar";
-
+import DashboardCalendar from "./DashboardCalendar";
 
 export default function Dashboard() {
   return (
     <div className="overflow-y-scroll max-h-[85vh]">
-      <h1 className="text-xl font-bold mb-4">Dashboard</h1>
+      <h1 className="text-xl font-bold mb-4">Welcome Admin</h1>
 
       <OverallStatsChart />
 
-      <div className="grid grid-cols-2">
-        <ProductBarChart />
-        <div className="flex items-center justify-center">
-          <Calendar />
-        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
         <ProductPieChart />
+        <DashboardCalendar />
+        <ProductBarChart />
         <ProductLineChart />
       </div>
     </div>
